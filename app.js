4fceebab26fff97185ec251f6745344ac10ef58a
Piero Toffanin <pt@masseranolabs.com>
2021-12-05 21:28:27 -0500

    Openings DB

--- app.js
+++ app.js
@@ -33,28 +33,60 @@ const calcDests = () => {
     return dests;
 }
 
+const uciToMove = (uci) => {
+    let result = ["", ""];
+    let c = 0;
+
+    for (let i = 0; i < uci.length; ){
+        if (/[a-h]/.test(uci[i])){
+            result[c] += uci[i++];
+            result[c++] += uci[i++];
+        }else{
+            i++; // Promotion character?
+        }
+        if (c > 1) break;
+    }
+    return result;
+};
+
 const playOtherSide = (orig, dest) => {
     // Update chess.js
     // Note: we don't check the return value here which would tell
     // us if this is a legal move.  That's because we only allowed legal moves by setting "dests"
     // on the board.
     game.move({from: orig, to: dest});
-    console.log("CALLED", orig, dest)
     
     cg.set({
-    // I'm not sure what this does! You can comment it out and not much changes
-    // turnColor: toColor(chess),
-    
-    // this highlights the checked king in red
-    check: game.in_check(),
-    
-    movable: {
-        // Only allow moves by whoevers turn it is
-        color: game.turn() === 'w' ? 'white' : 'black',
+        // I'm not sure what this does! You can comment it out and not much changes
+        // turnColor: toColor(chess),
         
-        // Only allow legal moves
-        dests: calcDests()
-    }
+        // this highlights the checked king in red
+        check: game.in_check(),
+        
+        movable: {
+            // Only allow moves by whoevers turn it is
+            color: game.turn() === 'w' ? 'white' : 'black',
+            
+            // Only allow legal moves
+            dests: calcDests()
+        }
+    });
+};
+
+const playMove = (orig, dest) => {
+    cg.move(orig, dest);
+    game.move({from: orig, to: dest});
+    cg.set({
+        // this highlights the checked king in red
+        check: game.in_check(),
+        
+        movable: {
+            // Only allow moves by whoevers turn it is
+            color: game.turn() === 'w' ? 'white' : 'black',
+            
+            // Only allow legal moves
+            dests: calcDests()
+        }
     });
 };
 
@@ -70,7 +102,18 @@ const cg = Chessground(domBoard, {
     }
 });
 
+const longOpenings = Openings.filter(o => o.uci.split(" ").length > 14);
+console.log(longOpenings);
+
+const opening = Openings.find(o => o.name === "Sicilian Defense: Dragon Variation, Yugoslav Attack, Soltis Variation");
+opening.uci.split(" ").forEach(m => {
+    const [orig, dest] = uciToMove(m);
+    playMove(orig, dest);
+});
+
 updateSize();
 window.addEventListener('resize', updateSize);
+
+window.uciToMove = uciToMove;
 
 }
