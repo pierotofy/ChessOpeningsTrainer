54d113963e7ea55bf50791480d14f863b60500b8
Piero Toffanin <pt@masseranolabs.com>
2022-07-02 12:44:00 -0400

    Vue webapp

---
+++ webapp/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    
+    <BoardTab :active="tab === 'explore'" mode="tree"/>
+    <BoardTab :active="tab === 'practice'" mode="treetrain"/>
+
+    <v-bottom-navigation
+      v-model="tab"
+      color="primary"
+      grow>
+      <v-btn value="explore" class="nav-btn">
+        <span>Explore</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+      <v-btn value="practice" class="nav-btn">
+        <span>Practice</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+      <v-btn value="list" class="nav-btn">
+        <span>List</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+
+<script>
+import BoardTab from './components/BoardTab';
+
+export default {
+  name: 'App',
+
+  components: {
+    BoardTab
+  },
+
+  data: () => ({
+    tab: 'explore'
+  }),
+};
+</script>
+
+<style>
+.v-bottom-navigation{
+  position: fixed !important;
+}
+.nav-btn{
+ height: 56px !important;
+ max-width: none !important;
+}
+.hide{
+  display: none;
+}
+.tab{
+  max-width: none;
+  padding: 0;
+}
+.board{
+  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  height: calc(100vh - 116px);
+  border: none;
+}
+</style>
---
+++ webapp/src/components/BoardTab.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container :class="{'hide': !active}" class="tab">
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <iframe class="board" :src="boardUrl"></iframe>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    props: ["active", "mode"],
+    data: () => ({
+      
+    }),
+    computed: {
+      boardUrl: function(){
+        const base = "/board/index.html";
+        const params = {
+          mode: this.mode
+        };
+
+        if (this.mode === "tree" || this.mode === "treetrain"){
+          params.maxTreeMoves = 5; // TODO
+        }
+
+        return `${base}?${Object.keys(params).map(k => k + '=' + params[k]).join('&')}`;
+      }
+    },
+    watch: {
+      active: function(){
+        // TODO load
+      }
+    }
+  }
+</script>
